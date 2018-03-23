352825a0795304e06641c2d89e561f6ca0f6c910
Roberto Santos <robertosantos@Robertos-MacBook-Pro.local>
2018-03-23 14:03:45 -0600

    updated files :sparkles:

---
+++ app.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const cors = require('cors')
+
+let students = [{
+    id: 1,
+    firstName: Alice,
+    lastName: Zephyr,
+    hometown: Seattle
+}, {
+    id: 2,
+    firstName: Bob,
+    lastName: Yellow,
+    hometown: Vancouver
+}, {
+    id: 3,
+    firstName: Claire,
+    lastName: Xylitol,
+    hometown: Toledo
+}, {
+    id: 4,
+    firstName: Daniel,
+    lastName: Winston,
+    hometown: Akron
+}, {
+    id: 5,
+    firstName: Edina,
+    lastName: Veritas,
+    hometown: Wichita
+}]
+
+function findById(data, id) {
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].id == id) {
+            return data[i]
+        }
+    }
+    return null
+}
+
+const app = express()
+app.use(cors())
+
+app.get('/', function(request, response) {
+    response.json({data: students})
+})
+
+app.get('/:id', function(request, response) {
+    let record = findById(students, request.params.id)
+    if (!record) {
+        response.status = 404
+        response.json({
+            error: {
+                message: 'No record found!'
+            }
+        })
+    }
+    response.json({data: record})
+})
+
+app.listen(process.env.PORT || 3000)
